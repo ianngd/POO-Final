dcf82139196da541af9e94b4bc86b258af838616
Iann Gabriel <74062836+ianngd@users.noreply.github.com>
2024-08-05 19:40:04 -0400

    Delete 002 directory

--- 002/testes.py
+++
@@ -1,9 +0,0 @@
-# No seu arquivo principal
-
-import aa1 as f
-
-projetos = []  # Lista para armazenar os projetos
-
-# Exemplo de uso da função cadastrar_projeto()
-projeto = f.cadastrar_projeto()
-projetos.append(projeto)
